0596d5dabc783bb3b3b990f6acb4c81dbc288ebb
doreancl <doreancl@gmail.com>
2022-01-03 14:34:31 -0300

    migration fix

--- composeexample/settings.py
+++ composeexample/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'api',
 ]
 
 MIDDLEWARE = [
@@ -75,9 +77,13 @@ WSGI_APPLICATION = 'composeexample.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'djongo',
+        'NAME': 'solobeer',
+        'ENFORCE_SCHEMA': False,
+        'CLIENT': {
+            'host': 'mongodb+srv://twitter-bot:twitter-bot@cluster0.xnwxg.mongodb.net/solobeer?retryWrites=true&w=majority'
+        }
+    },
 }
 
 
