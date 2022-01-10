2f0dbff517b689b9fbece0684dc2abb2a8d40a5b
doreancl <doreancl@gmail.com>
2022-01-09 23:29:49 -0300

    playing with viewset and celery task delay

--- api/views.py
+++ api/views.py
@@ -1,10 +1,22 @@
-from django.shortcuts import get_object_or_404
-from rest_framework import generics, viewsets
+from rest_framework import viewsets
+from rest_framework.decorators import action
 from rest_framework.response import Response
 
 from .models import Note
 from .serializers import NoteSerializer
+from .tasks import add
+
 
 class NoteViewSet(viewsets.ModelViewSet):
     queryset = Note.objects.all()
     serializer_class = NoteSerializer
+
+    @action(detail=False)
+    def add_job_ulala(self, request):
+        task = add.delay(
+            1, 2
+        )
+
+        return Response({
+            'task_id': task.id,
+        })
